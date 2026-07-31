2fa23cf665dd0870f416ddbf8810e252ca395a4f
Ahmad Meithaluny <meithaluny@gmail.com>
2026-07-31 18:10:34 +0300

    Add welcome message to root endpoint

--- Program.cs
+++ Program.cs
@@ -1,15 +1,14 @@
 ﻿using LinkManagerPro.Data;
 using LinkManagerPro.Models;
 using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddControllersWithViews();
 
-// Add DbContext
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Add DbContext with PostgreSQL
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -36,34 +35,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-// Create database
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.EnsureCreated();
-}
-// Create database and seed default user
-//using (var scope = app.Services.CreateScope())
-//{
-//    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-//    db.Database.EnsureCreated();
-
-//    // إنشاء مستخدم افتراضي إذا لم يكن موجوداً
-//    if (!db.Users.Any())
-//    {
-//        var defaultUser = new User
-//        {
-//            Id = 1,
-//            Username = "Admin",
-//            Email = "admin@example.com",
-//            PasswordHash = "hashed_password",
-//            CreatedAt = DateTime.UtcNow
-//        };
-//        db.Users.Add(defaultUser);
-//        db.SaveChanges();
-//    }
-//}
-
-
+// رسالة الترحيب
+app.MapGet("/", () => "LinkManagerPro API is running! Visit /Home/Index");
 
 app.Run();
